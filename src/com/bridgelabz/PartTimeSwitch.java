dcfb8b3f311147998dd5164bf7848cba4d604f21
kamal14rajput <kamal14rajput@gmail.com>
2021-12-20 02:22:45 +0530

    ADDED: parttime employee wage switch-case

---
+++ src/com/bridgelabz/PartTimeSwitch.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+public class PartTimeSwitch {
+
+	public static void main(String[] args) {
+		PartTime dailywage = new PartTime();
+		dailywage.empWages();
+	}
+}
+
+class PartTime {
+
+	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
+    public static int EMP_RATE_PER_HOUR = 20;
+    int empHrs = 0;
+    int empWage = 0;
+    
+    public void empWages() {
+    	int empCheck = (int)Math.floor(Math.random()*10)%3;
+    	switch (empCheck) {
+    		case IS_PART_TIME:
+    				empHrs = 4;
+    				break;
+    		case IS_FULL_TIME:
+            		empHrs = 8;
+            		break;
+    		default:
+    				empHrs = 0;
+}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);   	
+}
+}
+
