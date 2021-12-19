073acd2641e7a6e3dab5be636818cbcdc7c14775
kamal14rajput <kamal14rajput@gmail.com>
2021-12-20 02:24:47 +0530

    ADDED: calculate wages for month

---
+++ src/com/bridgelabz/WagesForMonth.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+public class WagesForMonth {
+
+	public static void main(String[] args) {
+		DailyWage dailywage = new DailyWage();
+		dailywage.empWages();
+	}
+}
+
+class DailyWage {
+
+	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
+    public static int EMP_RATE_PER_HOUR = 20;
+    public static int NUM_OF_WORKING_DAYS = 20;
+    int empHrs = 0;
+    int empWage = 0;
+    int totalEmpWage = 0;
+    
+    public void empWages() {
+    	for (int day = 0; day <  NUM_OF_WORKING_DAYS; day++) {
+    		int empCheck = (int)Math.floor(Math.random()*10)%3;
+    		switch (empCheck) {
+    			case IS_PART_TIME:
+    					empHrs = 4;
+    					break;
+    			case IS_FULL_TIME:
+    					empHrs = 8;
+    					break;
+    			default:
+    					empHrs = 0;
+    		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
+		System.out.println("Emp Wage: " + empWage);  
+    	}
+    	System.out.println("Total Emp Wage: " + totalEmpWage);
+    }
+}
